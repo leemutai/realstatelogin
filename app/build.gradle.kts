499ff272bd3cfd2959dacd99df9b5415e524a0b3
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-01-11 06:56:17 +0300

    commit c2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,13 @@ plugins {
 
 android {
     namespace = "com.example.real_estate"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.real_estate"
         minSdk = 24
-        targetSdk = 33
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -28,17 +29,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -48,6 +49,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
 
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/real_estate/MainActivity.kt
+++ app/src/main/java/com/example/real_estate/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.real_estate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        if (firebaseAuth.currentUser == null){
+            startLoginOptionsActivity()
+        }
+    }
+    private fun startLoginOptionsActivity(){
+        startActivity(Intent(this, LoginOptionsActivity::class.java))
     }
 }
