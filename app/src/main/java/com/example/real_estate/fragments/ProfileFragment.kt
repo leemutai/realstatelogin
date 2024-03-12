795bebed77f47b13c76d762517a628c0165ea2ec
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-03-12 16:07:15 +0300

    update profile fragment

--- app/src/main/java/com/example/real_estate/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/real_estate/fragments/ProfileFragment.kt
@@ -1,21 +1,43 @@
 package com.example.real_estate.fragments
 
+import android.app.ProgressDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.real_estate.R
+import com.example.real_estate.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
 
+//view binding
+    private lateinit var binding: FragmentProfileBinding
 
+    private val TAG = "PROFILE_TAG"
+    private lateinit var mContext: Context
+
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
+    }
+
+    private lateinit var progressDialog: ProgressDialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        progressDialog = ProgressDialog(mContext)
+        progressDialog.setTitle("")
     }
 }
